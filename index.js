2c3ca09d42401f612855b8421a325089df2e9826
Midori <thaistsushima@gmail.com>
2023-01-25 22:03:11 -0300

    Edited to create basic grid with JS and paint using mouse up down and enter

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
-//Open page
-//Load tools and grid
+//https://stackoverflow.com/questions/48593312/javascript-event-when-mouseover-and-mousedown
+
+
+for (let i = 0; i < 256; i++) {
+    let div = document.createElement("div");
+
+    //div.innerText = 'a';
+
+    document.getElementById("grid").appendChild(div);
+}
+
+document.getElementById("grid").style.gridTemplateColumns = "repeat(16, 1fr)";
+document.getElementById("grid").style.backgroundColor = "grey";
+
+let grid = document.getElementById("grid");
+let cel = grid.getElementsByTagName("div");
+let isToggling = false;
+
+function enableToggle(e) {
+    
+    console.log('enableToggle');
+    isToggling = true;
+
+    if (e.target !== grid) {
+        paint(e);
+    }
+}
+
+function disableToggle() {
+
+    console.log("disableToggle");
+    isToggling = false;
+}
+
+function paint(e) {
+
+    if (isToggling === false) {
+        return;
+    }
+
+    console.log('active');
+
+    e.target.classList.add('active');
+}
+
+function start() {
+    grid.onmousedown = enableToggle;
+
+    for (let i = 0, il = cel.length; i < il; i++) {
+        cel[i].onmouseenter = paint;
+    }
+
+    grid.onmouseup = disableToggle;
+}
+
+start();
